233bcb376b81177b6fd7683fb0e48c752418b0ac
piotr-markiewicz <piotr_markiewicz@epam.com>
2020-04-08 10:31:40 +0200

    small fixes

--- src/ntp.cpp
+++ src/ntp.cpp
@@ -14,9 +14,17 @@ uint32_t getTime(WiFiUDP& UDP) {
   if (UDP.parsePacket() == 0) { // If there's no response (yet)
     return 0;
   }
+  memset(NTPBuffer, 0, NTP_PACKET_SIZE);
   UDP.read(NTPBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
   // Combine the 4 timestamp bytes into one 32-bit number
-  uint32_t NTPTime = (NTPBuffer[40] << 24) | (NTPBuffer[41] << 16) | (NTPBuffer[42] << 8) | NTPBuffer[43];
+
+  // check version
+  if ((NTPBuffer[0] & 0b00111000) != 0b00100000) {
+    return 0;
+  }
+
+  uint32_t NTPTime = (NTPBuffer[40] << 24) | (NTPBuffer[41] << 16) | 
+                     (NTPBuffer[42] << 8) | NTPBuffer[43];
   // Convert NTP time to a UNIX timestamp:
   // Unix time starts on Jan 1 1970. That's 2208988800 seconds in NTP time:
   const uint32_t seventyYears = 2208988800UL;
@@ -33,9 +41,27 @@ bool sendNTPpacket(WiFiUDP& UDP) {
     return false;
   }
 
+      //                1                   2                   3
+      //  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9  0  1
+      // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      // |LI | VN  |Mode |    Stratum    |     Poll      |   Precision    |
+      // +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+      // LI - 3 - clock not sync'ed
+      // VN - 4
+      // mode - 3 - client
+
+
   memset(NTPBuffer, 0, NTP_PACKET_SIZE);  // set all bytes in the buffer to 0
   // Initialize values needed to form NTP request
-  NTPBuffer[0] = 0b11100011;   // LI, Version, Mode
+  NTPBuffer[0] = 0b11100011;   // LI, Version, Mode,
+  NTPBuffer[1] = 0;     // Stratum, or type of clock
+  NTPBuffer[2] = 6;     // Polling Interval
+  NTPBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  NTPBuffer[12]  = 49;
+  NTPBuffer[13]  = 0x4E;
+  NTPBuffer[14]  = 49;
+  NTPBuffer[15]  = 52;
   // send a packet requesting a timestamp:
   UDP.beginPacket(timeServerIP, UDP_PORT); // NTP requests are to port 123
   UDP.write(NTPBuffer, NTP_PACKET_SIZE);
